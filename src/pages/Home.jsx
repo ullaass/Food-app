afcbe4cff6b6ab4fffe5426780a74962cf34c6f5
Ullas Chandrakar <ullaschandrakar2404@gmail.com>
2023-12-01 01:57:36 +0530

    navbar complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import CategoryMenu from '../components/CategoryMenu'
 
 const Home = () => {
   return (
     <>
         <Navbar />
+        <CategoryMenu />
     </>
   )
 }
