14a0342d871fc81a7b81edd1f907578d2338dce2
Ullas Chandrakar <ullaschandrakar2404@gmail.com>
2023-12-03 12:52:27 +0530

    cart complte

--- src/components/FoodItems.jsx
+++ src/components/FoodItems.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import FoodCard from './FoodCard'
+import FoodData from '../data/FoodData'
 
 const FoodItems = () => {
   return (
-    <div>FoodItems</div>
+    <div className='flex flex-wrap gap-10 justify-center lg:justify-start mx-6 my-10'>
+      {FoodData.map((food)=>{
+        return <FoodCard 
+                  key={food.id} 
+                  id={food.id}
+                  name={food.name}
+                  price={food.price}
+                  desc={food.desc}  
+                  rating={food.rating}
+                  img={food.img}
+                  />
+      })}
+    </div>
   )
 }
 
